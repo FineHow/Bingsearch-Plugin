2fb25041d3d17526b53828a6174f6553c04c0beb
zwl <1434162587@qq.com>
2024-11-26 13:51:46 +0800

    ✨ feat: 修改apikey架构

--- src/pages/api/v2/_types.ts
+++ src/pages/api/v2/_types.ts
@@ -1,3 +1,7 @@
 export interface Settings {
   BING_API_KEY: string;
 }
+
+export const settings: Settings = {
+  BING_API_KEY: process.env.BING_API_KEY || "default_api_key", // 如果没有设置环境变量，使用默认值
+};
--- src/pages/api/v2/index.ts
+++ src/pages/api/v2/index.ts
@@ -1,21 +1,17 @@
 import {
   PluginErrorType,
   createErrorResponse,
-  getPluginSettingsFromRequest,
 } from '@lobehub/chat-plugin-sdk';
 
-import { Settings } from './_types';
+import { settings } from './_types';
 import runner from './_utils';
-
 export const config = {
   runtime: 'edge',
 };
 
 export default async (req: Request) => {
   // if (req.method !== 'POST') return createErrorResponse(PluginErrorType.MethodNotAllowed);
-
-  const settings = getPluginSettingsFromRequest<Settings>(req);
-
+  // const settings = getPluginSettingsFromRequest<Settings>(req);
   if (!settings)
     return createErrorResponse(PluginErrorType.PluginSettingsInvalid, {
       message: 'Plugin settings not found.',
