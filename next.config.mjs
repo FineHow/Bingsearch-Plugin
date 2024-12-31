2ce67d5b20dc478e9d661c9e9c126d8a409f1959
FINE <103298840+FineHow@users.noreply.github.com>
2024-12-31 13:18:16 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: ['@lobehub/ui'],
   async headers() {
-    return [
-      {
-        // matching all API routes
-        source: '/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+      return [
           {
-            key: 'Access-Control-Allow-Headers',
-            value:
-              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+              // matching all API routes
+              source: '/:path*',
+              headers: [
+                  { key: 'Access-Control-Allow-Credentials', value: 'true' },
+                  { key: 'Access-Control-Allow-Origin', value: '*' },
+                  { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+                  {
+                      key: 'Access-Control-Allow-Headers',
+                      value:
+                          'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version,x-lobe-trace,x-lobe-plugin-settings,x-lobe-chat-auth',
+                  },
+              ],
           },
-        ],
-      },
-    ];
+      ]
   },
-};
+}
 
-export default nextConfig;
+export default nextConfig
